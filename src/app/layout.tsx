49854c9d26cedb39bd596423c9f0652036766547
iPaulinh0 <pauloaraujo2607@gmail.com>
2025-05-04 08:52:36 -0300

    fix: sidebar declaration on app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { Toaster } from "sonner";
 import "./globals.css";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 const poppins = Poppins({
   subsets: ["latin"],
